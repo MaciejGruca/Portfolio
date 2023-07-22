a7454cdf9408da372c2e52469e36f23d46b194e6
Maciej Gruca <maciej1gruca@gmail.com>
2023-07-22 10:49:36 +0200

    Przykladowe funkcje bez parametrow

---
+++ Funkcje.js
@@ -0,0 +1,9 @@
+function hello(){
+    console.log("Welcome to Programiz!");
+}
+//function expression
+let  hello2 = function () {
+    console.log("Welcome to Programiz2!");
+};
+//arrow function
+let hello3 = () => console.log("Welcome to Programiz3!");
